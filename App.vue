8170c6d303cc77914f95cbe488d09ecacf781d07
PierMael <pier-mael.brelivet@efrei.net>
2020-04-25 18:39:22 +0200

    My

---
+++ App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>  
+    <h1>Clicked {{ timesClicked }} times.</h1>
+    <button @click="iAmClicked()">Click me!</button>
+  </div>
+</template>
+ 
+<script>
+    export default {
+        data() {
+            return {timesClicked: 0}
+        },
+        methods: {
+            iAmClicked() {
+                this.timesClicked +=1;
+            }
+        }
+    };
+</script> 
